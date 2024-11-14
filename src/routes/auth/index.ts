8864caf703a93bc52c698496b216ff8e1805cb4d
Xenos <acristyano22@gmail.com>
2024-11-14 10:43:31 -0800

    feat(auth): add sign-in

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,9 +1,11 @@
 import { Hono } from 'hono'
-import login from '../login'
 import discord from './discord'
 import github from './github'
+import login from './login'
+import signIn from './sign-in'
 
 export default new Hono()
   .route('/github', github)
   .route('/discord', discord)
   .route('/login', login)
+  .route('/sign-in', signIn)
--- src/routes/auth/login.ts
+++ src/routes/auth/login.ts
@@ -1,10 +1,9 @@
 import { db } from '@/db'
 import { User } from '@/db/schema'
-import { UserNotFoundError } from '@/errors/auth'
 import { generateToken } from '@/utils/generate-token'
 import { LoginSchema } from '@/validators/auth'
 import { zValidator } from '@hono/zod-validator'
-import bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt'
 import { eq } from 'drizzle-orm'
 import { Hono } from 'hono'
 
@@ -18,22 +17,25 @@ export default new Hono().post('/', zValidator('json', LoginSchema, (res, c) => 
     const { email, password } = c.req.valid('json') as { email: string, password: string }
     const user = (await db.select().from(User).where(eq(User.email, email))).at(0)
     if (!user) {
-      throw new UserNotFoundError('user not found')
+      return c.json({ message: 'User not found' }, 404)
     }
-    // eslint-disable-next-line ts/no-unsafe-assignment
-    const isPasswordCorrect = bcrypt.compareSync(password, user.password)
+
+    if (user.password === null) {
+      return c.json({ message: 'this Account Is Linked with a social account' }, 401)
+    }
+
+    const isPasswordCorrect = bcrypt.compareSync(password, user.password) as unknown as string
     if (Boolean(isPasswordCorrect) === false) {
-      throw new UserNotFoundError('password incorrect')
+      return c.json({ message: 'Incorrect password' }, 401)
     }
 
     const token = generateToken(user.id)
-
     return c.json({ user, token })
   }
   catch (error) {
-    if (!(error instanceof Error)) {
+    if (error instanceof Error) {
       return c.json({ message: 'Internal server error' }, 500)
     }
-    return c.json({ message: error.message }, 400)
+    return c.json({ message: 'Internal server error' }, 500)
   }
 })
