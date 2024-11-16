b88811b76bbab0f0c57ccb97f1aa92c0648afe3b
Xenos <acristyano22@gmail.com>
2024-11-16 03:48:28 -0800

    fix(naming convention): getPostById => deletePostById

--- src/features/post/lib/delete-post-by-id.ts
+++ src/features/post/lib/delete-post-by-id.ts
@@ -2,7 +2,7 @@ import { db } from '@/db'
 import { Post } from '@/db/schema'
 import { eq } from 'drizzle-orm'
 
-export default async function getPostById(id: string) {
+export default async function deletePostById(id: string) {
   const post = await db.delete(Post).where(eq(Post.id, id)).returning()
 
   return post[0] ?? null
