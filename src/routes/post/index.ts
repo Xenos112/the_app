ed86490514567218de39a9daf234695bd70eddbc
Xenos <acristyano22@gmail.com>
2024-11-16 04:18:36 -0800

    feat: user can now save the posts

--- src/routes/post/index.ts
+++ src/routes/post/index.ts
@@ -1,5 +1,5 @@
 import { db } from '@/db'
-import { Like, Post } from '@/db/schema'
+import { Like, Post, Save } from '@/db/schema'
 import getPostById from '@/features/post/lib/get-post-by-id'
 import validateToken from '@/utils/validate-token'
 import { uuid } from '@/validators'
@@ -174,10 +174,50 @@ export default new Hono()
         return c.json({ message: 'Post not found' }, 404)
       }
 
-      const isSavedByAuthenticatedUser = user ? await db.select().from(Post).where(and(eq(Post.id, id), eq(Post.author_id, user.id))).limit(1): []
+      const isSavedByAuthenticatedUser = user ? await db.select().from(Save).where(and(eq(Save.user_id, user.id), eq(Save.post_id, id))).limit(1): []
       return c.json({ saves: post.saves_count, saved: isSavedByAuthenticatedUser.length > 0 })
     }
     catch (error) {
       return c.json({ error }, 500)
+    }
+  })
+  .put('/:id/saves', zValidator('param',RouteValidator,(res,c) =>{
+    if(!res.success){
+      const errors = res.error.issues.map(error => error.message)
+      return c.json(errors, 400)
+    }
+  }),async (c) =>{
+    try {
+      const { id } = c.req.valid('param')
+      const token = getCookie(c, 'auth_token')
+      if (token === undefined) {
+        return c.json({ message: 'Unauthorized' }, 401)
+      }
+
+      const user = await validateToken(token)
+      if (!user) {
+        return c.json({ message: 'Unauthorized' }, 401)
+      }
+
+      const post = await getPostById(id)
+      if (post === null) {
+        return c.json({ message: 'Post not found' }, 404)
+      }
+
+      const postLiked = await db.select().from(Save).where(and(eq(Save.post_id, id), eq(Save.user_id, user.id))).limit(1)
+      if (postLiked.length > 0) {
+        return c.json({ liked: true })
+      }
+
+      await db.insert(Save).values({
+        post_id: post.id,
+        user_id: user.id,
+      })
+
+      await db.update(Post).set({ saves_count: post.saves_count! + 1  }).where(eq(Post.id, id))
+
+      return c.json({ saved: true })
+    } catch (error) {
+      return c.json('internal server error', 500)
     }
   })
