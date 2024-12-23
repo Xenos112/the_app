53220318eceb95c2ab97d555fa4de1ed471b7e47
Xenos <acristyano22@gmail.com>
2024-12-23 06:30:17 -0800

    fix id vidator for the saves delete route

--- src/controllers/post/get-post-by-id.ts
+++ src/controllers/post/get-post-by-id.ts
@@ -1,6 +1,10 @@
-import type validateToken from '@/utils/validate-token'
 import type { Context } from 'hono'
 import _getPostById from '@/features/post/lib/get-post-by-id'
+import { db } from '@/db'
+import { Like, Media, Save, type PostSelect } from '@/db/schema'
+import { and, eq } from 'drizzle-orm'
+import { getCookie } from 'hono/cookie'
+import validateToken from '@/utils/validate-token'
 
 type GetUserByIdContext = Context<object, '/:id', {
   in: {
@@ -18,13 +22,31 @@ type GetUserByIdContext = Context<object, '/:id', {
 export default async function getPostById(c: GetUserByIdContext) {
   try {
     const { id } = c.req.valid('param')
-    const post = await _getPostById(id)
-    if (post === null) {
-      return c.json({ message: 'Post not found' }, 404)
+    const authToken = getCookie(c, 'auth_token')
+    let userId: string | undefined
+
+    if (authToken) {
+      userId = (await validateToken(authToken))?.id
     }
-    return c.json(post)
+
+    const post = await _getPostById(id) as (PostSelect & { pictures?: { type: string, url: string }[] })
+    if (post === null) {
+      return c.json({ error: 'Post not found' }, 404)
+    }
+
+    const postPictures = await db.select({ type: Media.type, url: Media.url }).from(Media).where(eq(Media.target_id, id))
+    post.pictures = postPictures
+
+    if (!userId) {
+      return c.json({ data: { ...post, isLiked: false, isSaved: false } })
+    }
+
+    const isLiked = (await db.select().from(Like).where(and(eq(Like.post_id, id), eq(Like.user_id, userId))).limit(1)).length === 1
+    const isSaved = (await db.select().from(Save).where(and(eq(Save.post_id, id), eq(Save.user_id, userId))).limit(1)).length === 1
+    return c.json({ data: { ...post, isLiked, isSaved } })
   }
+
   catch (error) {
-    return c.json({ error })
+    return c.json({ error }, 500)
   }
 }
