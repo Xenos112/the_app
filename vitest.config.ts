489d1644cb9857780a703590154eef476dc378fe
Xenos112 <acristyano22@gmail.com>
2025-01-19 04:13:55 -0800

    add coverage for vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,10 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
+    coverage: {
+      provider: 'v8',
+      clean: true,
+    },
     include: ['src/**/*.test.ts'],
   },
 })
